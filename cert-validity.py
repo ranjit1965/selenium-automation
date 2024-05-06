c66a8296f1ab55d3a3017d1701987d95311381dc
Ranjit Sengupta <165263290+ranjit1965@users.noreply.github.com>
2024-05-06 10:07:27 +0530

    Update cert-validity.py

--- cert-validity.py
+++ cert-validity.py
@@ -20,37 +20,45 @@ def retrieve_current_until(cert_id):
         response = requests.get(url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
+            error_message = soup.find('p', class_='push-top alert alert-danger')
+            if error_message:
+                message_text = error_message.text.strip()
+                if "not a valid Certification ID" in message_text:
+                    return "invalid id"
+                elif "not mapped his or her ID to a redhat.com login" in message_text:
+                    return "not mapped yet"
             table = soup.find('table', {'style': 'width: 40%; white-space: nowrap'})
-            rows = table.find_all('tr')
-            for row in rows:
-                cells = row.find_all('td')
-                if len(cells) == 2 and cells[0].text.strip() == 'Current Until:':
-                    current_until = cells[1].text.strip()
-                    return current_until
-            return "invalid id"
+            if table:
+                rows = table.find_all('tr')
+                for row in rows:
+                    cells = row.find_all('td')
+                    if len(cells) == 2 and cells[0].text.strip() == 'Current Until:':
+                        return cells[1].text.strip()
+                return "expired"
+            else:
+                return "expired"
         else:
-            print("Cannot able to load the page please check internet connection.")
+            print(f"Error: {response.status_code} - Cannot able to load the page for ID {cert_id}")
+            return "invalid"
     except Exception as e:
-        print("ID is wrong")
-        return "invalid id"
-        
+        print(f"Error: {e}")
+        return "invalid"
+
+
+#print(retrieve_current_until('230-223-091'))
+   
 for row in range(2, end_row+1):
     print(f'Executing {row-1} id')
-    try:
-        cell=f'{cert_col}{row}'
-        cert_id=ws[cell].value
-        valid_cur_row=f'{valid_col}{row}'
-        if cert_id is None:
-            ws[valid_cur_row]='invalid id'
-            continue
-        valid_date=retrieve_current_until(cert_id)
-        ws[valid_cur_row]=valid_date
-    except Exception as e:
-        print(e)
-        wb.save(book_name)
+    cell=f'{cert_col}{row}'
+    cert_id=str(ws[cell].value)
+    cert_id=cert_id.replace(' ','')
+    valid_cur_row=f'{valid_col}{row}'
+    if cert_id is None:
+        ws[valid_cur_row]='invalid id'
+        continue
+    valid_date=retrieve_current_until(cert_id)
+    ws[valid_cur_row]=valid_date
 
 wb.save(book_name)
     
-
-
 
